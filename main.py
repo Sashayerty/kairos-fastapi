541999435469ef14363e78f07e8c34a126109996
Alex <Sashayerty@ya.ru>
2025-05-05 17:44:37 +0300

    Удалены зависимости от Flask

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.wsgi import WSGIMiddleware
 
 from app import kairos
-from flask_version import app as flask_app
 
 app = FastAPI(title="Kairos FastAPI Ver")
 app.include_router(router=kairos)
-app.mount("/", WSGIMiddleware(flask_app))
